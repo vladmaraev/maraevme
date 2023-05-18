248a6716a8158828ea11aedb39fa12938ba96932
Vladislav Maraev <vlad@maraev.me>
2023-05-18 12:34:58 +0200

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,39 @@
 module.exports = {
-  siteMetadata: {
-    title: "Vladislav Maraev",
-  },
-  plugins: [
-      "gatsby-plugin-sass",
-      "gatsby-plugin-react-helmet",
-      "gatsby-plugin-gatsby-cloud",
-      `gatsby-plugin-image`,
-      `gatsby-plugin-sharp`,
-      `gatsby-transformer-sharp`,
-      `gatsby-transformer-orga`,
-      {
-	  resolve: "gatsby-plugin-google-analytics",
-	  options: {
-              trackingId: "UA-64561587-1",
-	  },
-      },
-      {
-	  resolve: `gatsby-source-filesystem`,
-	  options: {
-	      name: `org`,
-	      path: `${__dirname}/src/assets/org`,
-	  },
-      },
-      {
-	  resolve: `gatsby-source-filesystem`,
-	  options: {
-	      name: `publications`,
-	      path: `${__dirname}/src/assets/publications/`,
-	  },
-      },
-      'gatsby-transformer-bibtex',
-  ],
+    siteMetadata: {
+        title: "Vladislav Maraev",
+    },
+    plugins: [
+        "gatsby-plugin-sass",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-gatsby-cloud",
+        `gatsby-plugin-image`,
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        `gatsby-transformer-orga`,
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                // You can add multiple tracking ids and a pageview event will be fired for all of them.
+                trackingIds: [
+                    "G-2T0FC6J2WF", // Google Analytics / GA
+                ],
+            },
+        },
+
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `org`,
+                path: `${__dirname}/src/assets/org`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `publications`,
+                path: `${__dirname}/src/assets/publications/`,
+            },
+        },
+        "gatsby-transformer-bibtex",
+    ],
 };
